a1ac670de9aabad5ba01909bff5c62c787162b0b
xsuto <michal.sutowicz@gmail.com>
2024-03-06 12:04:51 +0100

    Add initial project files and configurations

---
+++ README.md
@@ -0,0 +1,21 @@
+
+# Requirements
+Ensure you have the following prerequisites installed before getting started:
+
+- **Node 20 or higher**: Make sure you have Node.js installed, with a version of 20 or higher. You can download it [here](https://nodejs.org/).
+- **pnpm**: If not already installed, you can do so with the command:
+  ```bash
+  npm install -g pnpm
+  ```
+
+# Installation
+1. Clone the repository
+2. Navigate to the project's root directory and run the following command to install dependencies
+```bash
+pnpm install
+```
+3. Create .env file, copy content from .env.example and fill in the required fields
+
+# Usage
+- Run the project with `pnpm dev`
+- Build the project with `pnpm build`
---
+++ src/lib/auth.ts
@@ -0,0 +1,75 @@
+import { cache } from "react";
+import { Lucia } from "lucia";
+import { GitHub } from "arctic";
+import { PrismaAdapter } from "@lucia-auth/adapter-prisma";
+import { env } from "~/env";
+import { db } from "~/server/db";
+import { cookies } from "next/headers";
+
+import type { Session, User } from "lucia";
+import type { User as DbUser } from "@prisma/client";
+
+const adapter = new PrismaAdapter(db.session, db.user);
+
+export const lucia = new Lucia(adapter, {
+  sessionCookie: {
+    attributes: {
+      secure: process.env.NODE_ENV === "production",
+    },
+  },
+  getUserAttributes: (attributes) => {
+    return {
+      username: attributes.username,
+      avatarUrl: attributes.avatarUrl,
+      githubId: attributes.githubId,
+    };
+  },
+});
+
+declare module "lucia" {
+  interface Register {
+    Lucia: typeof lucia;
+    DatabaseUserAttributes: Omit<DbUser, "id">;
+  }
+}
+
+export const validateRequest = cache(
+  async (): Promise<
+    { user: User; session: Session } | { user: null; session: null }
+  > => {
+    const sessionId = cookies().get(lucia.sessionCookieName)?.value ?? null;
+    if (!sessionId) {
+      return {
+        user: null,
+        session: null,
+      };
+    }
+
+    const result = await lucia.validateSession(sessionId);
+    // next.js throws when you attempt to set cookie when rendering page
+    try {
+      if (result.session?.fresh) {
+        const sessionCookie = lucia.createSessionCookie(result.session.id);
+        cookies().set(
+          sessionCookie.name,
+          sessionCookie.value,
+          sessionCookie.attributes,
+        );
+      }
+      if (!result.session) {
+        const sessionCookie = lucia.createBlankSessionCookie();
+        cookies().set(
+          sessionCookie.name,
+          sessionCookie.value,
+          sessionCookie.attributes,
+        );
+      }
+    } catch {}
+    return result;
+  },
+);
+
+export const github = new GitHub(
+  env.GITHUB_CLIENT_ID,
+  env.GITHUB_CLIENT_SECRET,
+);
