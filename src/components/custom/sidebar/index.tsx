5e68e8c808cb2b9598f7d6307833c8a34d14b3c5
AdamPar <adamparkitny7@gmail.com>
2024-03-10 21:42:48 +0100

    display project author and users

--- src/components/custom/sidebar/index.tsx
+++ src/components/custom/sidebar/index.tsx
@@ -6,17 +6,25 @@ import { DisplayProjects } from "./DisplayProjects";
 
 export default async function Sidebar({ user }: { user: User}) {
   const projects = await db.project.findMany({
-    where: {
+    where:{
       users: {
         some: {
           id: user.id,
         },
       },
     },
+    include:{
+      author:{
+        select:{username: true}
+      },
+      _count:{
+        select:{users: true}
+      }
+    }
   });
 
   return (
-    <nav className="h-screen w-96 border-r border-gray-700">
+    <nav className="h-screen min-w-96 max-w-96 border-r border-gray-700">
       <div className="flex h-full flex-col justify-between px-6 pt-12">
         <div>
           <DisplayUser user={user} />
