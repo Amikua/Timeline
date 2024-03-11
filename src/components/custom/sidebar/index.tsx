434f04c8b486ceee73fbe765a39c8a039336d1ae
xsuto <michal.sutowicz@gmail.com>
2024-03-11 17:44:46 +0100

    Add husky and increase project area in sidebar

--- src/components/custom/sidebar/index.tsx
+++ src/components/custom/sidebar/index.tsx
@@ -4,21 +4,21 @@ import { type User } from "lucia";
 import { DisplayUser } from "./DisplayUser";
 import { DisplayProjects } from "./DisplayProjects";
 
-export default async function Sidebar({ user }: { user: User}) {
+export default async function Sidebar({ user }: { user: User }) {
   const projects = await db.project.findMany({
-    where:{
+    where: {
       users: {
         some: {
           id: user.id,
         },
       },
     },
-    include:{
-      author:{
-        select:{username: true}
+    include: {
+      author: {
+        select: { username: true }
       },
-      _count:{
-        select:{users: true}
+      _count: {
+        select: { users: true }
       }
     }
   });
@@ -26,11 +26,11 @@ export default async function Sidebar({ user }: { user: User}) {
   return (
     <nav className="relative h-screen min-w-96 max-w-96 border-r border-gray-700">
       <div className="flex h-full max-h-full flex-col justify-between px-6 pt-12">
-        <div className="flex flex-col gap-6 max-h-[78%]">
+        <div className="flex flex-col gap-6 max-h-[90%]">
           <DisplayUser user={user} />
           <DisplayProjects projects={projects} />
         </div>
-        <AddProject/>
+        <AddProject />
       </div>
     </nav>
   );
