3df31e46fa181cc3f773efaad607093932d0143d
xsuto <michal.sutowicz@gmail.com>
2024-04-14 14:43:29 +0200

    Show add new project if user doesn't have any project

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,32 @@
-export default async function Page() {
-	return (
-		<>
-		</>
-	);
-}
+import { redirect } from "next/navigation";
+import { AddProject } from "~/components/custom/sidebar/AddProject";
+import { validateRequest } from "~/lib/auth";
+import { db } from "~/server/db";
 
+export default async function Page() {
+  const { user } = await validateRequest();
+  if (!user) {
+    return redirect("/dashboard");
+  }
+
+  const userProjectsCount = await db.project.count({
+    where: {
+      users: {
+        some: {
+          id: user.id,
+        },
+      },
+    },
+  });
+
+  if (userProjectsCount !== 0) {
+    return <></>;
+  }
+
+  return (
+    <div className="flex h-full min-h-screen w-full flex-col items-center justify-center gap-8">
+      <h1 className="text-3xl font-thin">Start by creating new project</h1>
+      <AddProject />
+    </div>
+  );
+}
