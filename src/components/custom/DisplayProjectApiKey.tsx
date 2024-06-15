7ee5b6eea9af7b9c4e93e6fd666a546a9190ff53
xsuto <michal.sutowicz@gmail.com>
2024-06-15 17:01:14 +0200

    Show loading spinner when creating/updating api key

--- src/components/custom/DisplayProjectApiKey.tsx
+++ src/components/custom/DisplayProjectApiKey.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import { Button } from "../ui/button";
 import { createOrUpdateApiKey } from "./actions";
+import { Loader2 } from "lucide-react";
 
 export function DisplayApiKey({
   apiKey,
@@ -12,13 +13,18 @@ export function DisplayApiKey({
   projectId: string;
 }) {
   const [hidden, setHidden] = useState(true);
+  const [isCreatingApiKey, setIsCreatingApiKey] = useState(false);
 
   return (
     <div className="flex flex-col gap-4">
       <h1 className="text-2xl font-bold">Project api key: </h1>
       {apiKey ? (
-      <h2 className="text-lg">{hidden ? "*".repeat(apiKey.length) : apiKey}</h2>
-      ): <h2 className="text-lg">No api key found</h2>}
+        <h2 className="text-lg">
+          {hidden ? "*".repeat(apiKey.length) : apiKey}
+        </h2>
+      ) : (
+        <h2 className="text-lg">No api key found</h2>
+      )}
       <div className="space-x-4">
         {apiKey ? (
           <Button
@@ -33,10 +39,21 @@ export function DisplayApiKey({
           variant="outline"
           className="h-12 w-36"
           onClick={async () => {
+            setIsCreatingApiKey(true);
             await createOrUpdateApiKey({ projectId });
+            setIsCreatingApiKey(false);
           }}
         >
-          { apiKey ? "Generate new key" : "Create new key" }
+          {isCreatingApiKey ? (
+            <>
+              <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+              Please wait...
+            </>
+          ) : apiKey ? (
+            "Generate new key"
+          ) : (
+            "Create new key"
+          )}
         </Button>
       </div>
     </div>
